e241feaab8bb9ace3546518b38d2932a59c306e1
Kaiyin Zhong <k.zhong@ar-development.nl>
2018-03-01 22:56:47 +0100

    add annotation to image

--- merge_images.sh
+++ merge_images.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 
-montage sigmoidfc*.png -tile 7x1 -geometry +3+3 sigmoid.png && rm sigmoidfc*.png
-montage    tanhfc*.png -tile 7x1 -geometry +3+3    tanh.png && rm    tanhfc*.png
-montage    relufc*.png -tile 7x1 -geometry +3+3    relu.png && rm    relufc*.png
-montage    sigmoid.png \
-	      tanh.png \
-	      relu.png -tile 3x1 -geometry +5+5     all.png
+
+montage sigmoidfc*.png -tile 7x1 -geometry +3+3 sigmoid.png
+montage    tanhfc*.png -tile 7x1 -geometry +3+3    tanh.png
+montage    relufc*.png -tile 7x1 -geometry +3+3    relu.png
+convert sigmoid.png  -background Khaki  label:'sigmoid' -gravity Center -append    sigmoid_anno.png
+convert tanh.png     -background Khaki  label:'tanh'    -gravity Center -append       tanh_anno.png
+convert relu.png     -background Khaki  label:'relu'    -gravity Center -append       relu_anno.png
+montage    sigmoid_anno.png \
+	      tanh_anno.png \
+	      relu_anno.png -tile 3x1 -geometry +5+5     all.png
